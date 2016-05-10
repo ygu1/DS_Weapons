9c27bd2596a3cceaed04c0d6e858d09d76030574
Yanliang Gu <Yanliang@yanliangs-macbook-pro.local>
2016-05-10 02:01:59 -0400

    Initial Commit

---
+++ DS Weapons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DS Weapons
+//
+//  Created by Yanliang Gu on 5/10/16.
+//  Copyright © 2016 Yanliang Gu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DS WeaponsTests/DS_WeaponsTests.swift
@@ -0,0 +1,36 @@
+//
+//  DS_WeaponsTests.swift
+//  DS WeaponsTests
+//
+//  Created by Yanliang Gu on 5/10/16.
+//  Copyright © 2016 Yanliang Gu. All rights reserved.
+//
+
+import XCTest
+@testable import DS_Weapons
+
+class DS_WeaponsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
